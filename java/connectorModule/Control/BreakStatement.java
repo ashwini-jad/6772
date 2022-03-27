3e520eff0da9fbda485482a774f4e1a4b24bc3f0
xyz <xyz@xyz-PC>
2022-03-27 23:01:30 +0530

    First commit

---
+++ java/connectorModule/Control/BreakStatement.java
@@ -0,0 +1,24 @@
+package Control;
+
+public class BreakStatement {
+
+	public static void main(String[] args) {
+   int i=1;
+   while(i<=10)
+   {
+	   System.out.println("\n" +i);
+	   i++;
+	   if(i==6)
+		   {
+		   
+		   break;
+		   }
+		   }
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ java/connectorModule/Control/forloop.java
@@ -0,0 +1,14 @@
+package Control;
+
+public class forloop {
+
+	public static void main(String[] args) {
+int i;
+for(i=0;i<5;i++)
+{
+System.out.println("\n Example of for loop");
+
+}
+	}
+
+}
